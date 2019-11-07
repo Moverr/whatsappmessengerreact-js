493d1eec9e95603c45bbc51e929efc0293f8939a
Muyinda Rogers <moverr@gmail.com>
2019-11-07 17:36:13 +0300

    Adding Bootstrap :

---
+++ src/components/WhatsappChartUI.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class WhatsappChartUI extends Component {
+    static propTypes = {
+        prop: PropTypes
+    }
+
+    render() {
+        return (
+            <div>
+                <h1> Testing me out </h1>
+            </div>
+        )
+    }
+}
+
+export default WhatsappChartUI;
